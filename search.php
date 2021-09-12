84830cbb91632e859de9750169730b4756b3f44d
Weno-Hans <66069906+Weno-Hans@users.noreply.github.com>
2021-09-12 04:24:49 +0100

    Add files via upload

--- search.php
+++ search.php
@@ -13,20 +13,30 @@
             <div class="col-md-8">
 
             <?php 
-            $query = "SELECT * FROM posts";
-            $select_all_posts_query = mysqli_query($connection, $query);
 
-                while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+            if(isset($_POST['submit'])) {
+                $search = $_POST['search'];
+
+                $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+                $search_query = mysqli_query($connection, $query);
+
+                if(!$search_query) {
+                    die("Query Failed" . mysqli_error($connection));
+                }
+
+                $count = mysqli_num_rows($search_query);
+                if($count == 0) {
+                    echo "<h1>NO RESULT</h1>";
+                } else {
+                   
+                while($row = mysqli_fetch_assoc($search_query)) {
                     $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
                     $post_content = $row['post_content'];
                     ?>
-
-
-
-<h1 class="page-header">
+                <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
@@ -44,10 +54,13 @@
                 <hr>
                 <p><?php echo $post_content; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
                 <hr>
+               <?php }
+                }
+            } 
+            ?>
 
-               <?php }?>
+            
 
                 
             
