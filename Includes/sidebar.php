0d9abd460b61dd8612aa74f368c914137cce1ff9
Weno-Hans <66069906+Weno-Hans@users.noreply.github.com>
2021-09-13 05:08:26 +0100

    Add files via upload

--- Includes/sidebar.php
+++ Includes/sidebar.php
@@ -22,6 +22,26 @@
 </div>
 
 
+<!-- Login -->
+<div class="well">
+    <h4>Login</h4>
+    <form action="Includes/login.php" method="POST">
+    <div class="form-group">
+        <input name="username" type="text" class="form-control" placeholder="Enter Username">  
+    </div>
+
+    <div class="input-group">
+        <input name="password" type="password" class="form-control" placeholder="Enter Password">
+        <span class="input-group-btn">
+            <button class="btn btn-primary" name="login" type="submit">Submit</button>
+        </span>  
+    </div>
+
+    </form> 
+    <!-- /.input-group -->
+</div>
+
+
 
 
 
@@ -47,8 +67,9 @@ $select_categories_sidebar = mysqli_query($connection, $query);
             <?php
             while($row = mysqli_fetch_assoc($select_categories_sidebar)) {
                 $cat_title = $row['cat_title'];
+                $cat_id = $row['cat_id'];
             
-                echo "<li><a href=''>{$cat_title}</a></li>";
+                echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
             }
             ?>
 
