71bcbdaec79a17fa0ae01a40cea2267991497b2e
Weno-Hans <66069906+Weno-Hans@users.noreply.github.com>
2021-09-14 15:26:31 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -25,9 +25,7 @@
                     $post_content = substr($row['post_content'], 0, 100);
                     $post_status = $row['post_status'];
 
-                    if($post_status !== 'published') {
-                        echo "<h1 class='text-center'>NO POST SORRY</h1>";
-                    } else {
+                    if($post_status == 'published') {
 
                     
 
@@ -49,12 +47,14 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
                 <hr>
+
                 <a href="post.php?p_id=<?php echo $post_id; ?>">
                 <img class="img-responsive" src="Images/<?php echo $post_image; ?>" alt="">
                 </a>
+
                 <hr>
                 <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
 
