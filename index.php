2058a441a2769c8dca9b00cf3bb2e7698b10e6c5
Weno-Hans <66069906+Weno-Hans@users.noreply.github.com>
2021-09-20 19:56:46 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -119,7 +119,14 @@
         <?php
 
         for($i = 1; $i <= $count; $i++) {
-            echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+
+            if($i==$page) {
+                echo "<li><a class = 'active_link' href='index.php?page={$i}'>{$i}</a></li>";
+            } else {
+                echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+            }
+
+            
         }
 
         ?>
