f219eb12637b0c592abb4e741914fbe4a4fba426
Weno-Hans <66069906+Weno-Hans@users.noreply.github.com>
2021-09-27 14:44:47 +0100

    Add files via upload

--- Includes/login.php
+++ Includes/login.php
@@ -1,58 +1,15 @@
 <?php
-
-include "db.php";
 session_start();
+include "db.php";
+include "../admin/functions.php";
+
 
 if(isset($_POST['login'])) {
-    //global $connection;
-    $username = $_POST['username'];
-    $password = $_POST['password'];
 
-    $username = mysqli_real_escape_string($connection, $username);
-    $password = mysqli_real_escape_string($connection, $password);
+    login_user($_POST['username'], $_POST['password']);
 
 
-
-
-
-
-    //This is where i adjusted the login so that only users who are admins can login into the admin section by adding AND user_role = 'admin' to the query below
-
-
-
-
-
-    $query = "SELECT * FROM users WHERE username = '{$username}' ";
-    $select_user_query = mysqli_query($connection, $query);
-    if (!$select_user_query) {
-        die("QUERY FAILED". mysqli_error($connection));
-    }
-
-    while($row = mysqli_fetch_array($select_user_query)) {
-        $db_user_id = $row['user_id'];
-        $db_username = $row['username'];
-        $db_user_password = $row['user_password'];
-        $db_user_firstname = $row['user_firstname'];
-        $db_user_lastname = $row['user_lastname'];
-        $db_user_role = $row['user_role'];
-    }
-
-    //$password = crypt($password, $db_user_password);
-
-    if (password_verify($password, $db_user_password)) {
-
-        $_SESSION['username'] = $db_username;
-        $_SESSION['firstname'] = $db_user_firstname;
-        $_SESSION['lastname'] = $db_user_lastname;
-        $_SESSION['user_role'] = $db_user_role;
-
-        header("Location: ../admin");
-
-       
-
-    } else {
-        header("Location: ../index.php");
-    }
+   
 }
 
 
