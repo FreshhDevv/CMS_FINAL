11ec32eccf411fa5909fefe4c8fe92d6d67f170b
FreshDev <billyhans90@gmail.com>
2022-04-28 16:23:35 +0100

    Refactoring

--- Includes/navigation.php
+++ Includes/navigation.php
@@ -1,24 +1,24 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/PHP Workspace/CMS_FINAL/">CMS Front</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/PHP Workspace/CMS_FINAL/">CMS Front</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
 
                 <?php
                 $query = "SELECT * FROM categories LIMIT 2";
                 $select_all_categories_query = mysqli_query($connection, $query);
 
-                while($row = mysqli_fetch_assoc($select_all_categories_query)) {
+                while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
                     $cat_title = $row['cat_title'];
                     $cat_id = $row['cat_id'];
 
@@ -30,33 +30,30 @@
 
                     $registration = 'registration.php';
 
-                    if(isset($_GET['category']) && $_GET['category'] == $cat_id) {
+                    if (isset($_GET['category']) && $_GET['category'] == $cat_id) {
 
                         $category_class = 'active';
-
                     } else if ($pageName == $registration) {
 
                         $registration_class = 'active';
-
                     }
- 
+
                     echo "<li class = '$category_class'><a href='/PHP Workspace/CMS_FINAL/category/{$cat_id}'>{$cat_title}</a></li>";
-
-
                 }
 
                 ?>
 
-                <?php if(isLoggedIn()): ?>
+                <?php if (isLoggedIn()) : ?>
 
                     <li>
                         <a href="/PHP Workspace/CMS_FINAL/admin">Admin</a>
+                        
                     </li>
                     <li>
                         <a href="/PHP Workspace/CMS_FINAL/includes/logout">Logout</a>
                     </li>
 
-                    <?php else: ?>
+                <?php else : ?>
 
                     <li>
                         <a href="/PHP Workspace/CMS_FINAL/login">Login</a>
@@ -65,35 +62,34 @@
                 <?php endif; ?>
 
 
-                    
-                   
-                    <li class='<?php echo $registration_class; ?>'>
-                        <a href="/PHP Workspace/CMS_FINAL/registration">Registration</a>
-                    </li>
-                    <li>
-                        <a href="/PHP Workspace/CMS_FINAL/contact">Contact</a>
-                    </li>
 
-                    <?php
-                    //Restarting the sessions
-                    if (session_status() == PHP_SESSION_NONE) session_start();
 
-                    if(isset($_SESSION['user_role'])) {
-                        if(isset($_GET['p_id'])) {
+                <li class='<?php echo $registration_class; ?>'>
+                    <a href="/PHP Workspace/CMS_FINAL/registration">Registration</a>
+                </li>
+                <li>
+                    <a href="/PHP Workspace/CMS_FINAL/contact">Contact</a>
+                </li>
+
+                <?php
+                //Restarting the sessions
+                if (session_status() == PHP_SESSION_NONE) session_start();
+
+                if (isset($_SESSION['user_role'])) {
+                    if (isset($_GET['p_id'])) {
                         $the_post_id = $_GET['p_id'];
 
                         echo "<li><a href='/PHP Workspace/CMS_FINAL/admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
-                        }
-                        
                     }
+                }
 
-                    ?>
-                   
+                ?>
 
 
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+
+            </ul>
         </div>
-        <!-- /.container -->
-    </nav>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
